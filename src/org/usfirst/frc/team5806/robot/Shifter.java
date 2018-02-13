8f34e61cf7b3167c5e8352e1765d044a8ba4e7b4
ja-San <jake_sanders@horacemann.org>
2018-02-12 19:01:23 -0500

    Shifter updates, style consistency
    Shifter: made highGear private, and added get function. Also added
    boolean to constructor for starting in low versus high gear. Also did
    consistency updates in terms of style

--- src/org/usfirst/frc/team5806/robot/Shifter.java
+++ src/org/usfirst/frc/team5806/robot/Shifter.java
@@ -1,25 +1,27 @@
 package org.usfirst.frc.team5806.robot;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
-public class Shifter 
-{
+public class Shifter {
 	private DoubleSolenoid solshift;
-	public boolean highGear;
-	
-	public Shifter(int port1, int port2)
-	{
+	private boolean highGear;
+
+	public Shifter(int port1, int port2, boolean startInHighGear) {
 		solshift = new DoubleSolenoid(port1, port2);
-		highGear = false;
-		solshift.set(DoubleSolenoid.Value.kReverse);
+		highGear = startInHighGear ? true : false;
+		solshift.set(startInHighGear ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
 	}
-	public void high()
-	{
+
+	public void high() {
 		highGear = true;
 		solshift.set(DoubleSolenoid.Value.kForward);
 	}
-	public void low()
-	{
+
+	public void low() {
 		highGear = false;
 		solshift.set(DoubleSolenoid.Value.kReverse);
 	}
+
+    public boolean getIfHighGear() {
+        return highGear;
+    }
 }
--- src/org/usfirst/frc/team5806/robot/Winch.java
+++ src/org/usfirst/frc/team5806/robot/Winch.java
@@ -1,42 +1,33 @@
 package org.usfirst.frc.team5806.robot;
 import edu.wpi.first.wpilibj.VictorSP;
 
-public class Winch
-{
+public class Winch {
 	private VictorSP motor;
-	private WinchState currentstate;
+	private WinchState currentState;
 	private static final double UP_SPEED = 1.0;
-	
-	public Winch(int port)
-	{
+
+	public Winch(int port) {
 		motor = new VictorSP(port);
-		currentstate = WinchState.STOP;
+		currentState = WinchState.STOP;
 	}
-	
-	public void pull()
-	{
-		currentstate = WinchState.UP;
+
+	public void pull() {
+		currentState = WinchState.UP;
 	}
-	
-	public void stop()
-	{
-		currentstate = WinchState.STOP;
+
+	public void stop() {
+		currentState = WinchState.STOP;
 	}
-	
-	public void update()
-	{
-		if (currentstate == WinchState.UP)
-		{
+
+	public void update() {
+		if (currentState == WinchState.UP) {
 			motor.set(UP_SPEED);
-		}
-		else
-		{
+		} else {
 			motor.set(0);
 		}
 	}
 }
 
-enum WinchState 
-{
+enum WinchState {
 	UP, STOP
 }
