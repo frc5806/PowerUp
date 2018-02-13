a89dfdd77c5c08b32122be8d88cb02ed5c51b0b7
Definately Not Bradley <Its not bradley>
2018-02-13 15:36:08 -0500

    Added Grabber

---
+++ src/org/usfirst/frc/team5806/robot/Grabber.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team5806.robot;
+
+import edu.wpi.first.wpilibj.VictorSP;
+
+public class Grabber 
+{
+	private final double SPEED = 1;
+	private VictorSP grab;
+	private GrabPositions position;
+	public Grabber(int port)
+	{
+		grab = new VictorSP(port);
+		position = GrabPositions.STOP;
+	}
+	public void up()
+	{
+		position = GrabPositions.UP;
+	}
+	public void down()
+	{
+		position = GrabPositions.DOWN;
+	}
+	public void update() {
+		switch (position){
+		case UP:
+			grab.set(SPEED);
+		
+		case DOWN:
+			grab.set(-SPEED);
+		
+		case STOP:
+			grab.set(0);
+	
+		}
+	}
+}
+enum GrabPositions{
+	UP,DOWN,STOP
+}
