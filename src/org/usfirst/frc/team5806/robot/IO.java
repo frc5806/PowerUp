f99c9012a287885f6fc90cd9f85ba508c886d2cd
Michael Truell <truell20@gmail.com>
2018-02-15 18:07:52 -0500

    changeIO

--- src/org/usfirst/frc/team5806/robot/IO.java
+++ src/org/usfirst/frc/team5806/robot/IO.java
@@ -9,34 +9,35 @@ public class IO {
 	private double motorSpeed;
 	private IOMotorState currentState = IOMotorState.OFF;
 
-	private DoubleSolenoid left; //Left flap solenoid
-	private DoubleSolenoid right; //Right flap solenoid
+	private DoubleSolenoid leftFlapPiston; //Left flap solenoid
+	private DoubleSolenoid rightFlapPiston; //Right flap solenoid
 
 	private DoubleSolenoid leftPopper;
 	private DoubleSolenoid rightPopper;
 
-	private VictorSP leftWall;
-	private VictorSP leftFlap;
-	private VictorSP rightWall;
-	private VictorSP rightFlap;
+	private VictorSP leftWallMotor;
+	private VictorSP leftFlapMotor;
+	private VictorSP rightWallMotor;
+	private VictorSP rightFlapMotor;
 
-	public IO(DoubleSolenoid left, DoubleSolenoid right, 
+	public IO(DoubleSolenoid leftFlapPiston, DoubleSolenoid rightFlapPiston, 
 		DoubleSolenoid leftPopper, DoubleSolenoid rightPopper, 
-		VictorSP leftWall, VictorSP rightWall,
-		VictorSP leftFlap, VictorSP rightFlap) {
+		VictorSP leftWallMotor, VictorSP rightWallMotor,
+		VictorSP leftFlapMotor, VictorSP rightFlapMotor, 
+		double initialMotorSpeed) {
 		motorSpeed = initialMotorSpeed;
 
-		this.left = left;
-		this.right = right;
+		this.leftFlapPiston = leftFlapPiston;
+		this.rightFlapPiston = rightFlapPiston;
 
 		this.leftPopper = leftPopper;
 		this.rightPopper = rightPopper;
 
-		this.leftWall = leftWall;
-		this.rightWall = rightWall;
+		this.leftWallMotor = leftWallMotor;
+		this.rightWallMotor = rightWallMotor;
 
-		this.leftFlap = leftFlap;
-		this.rightFlap = rightFlap;
+		this.leftFlapMotor = leftFlapMotor;
+		this.rightFlapMotor = rightFlapMotor;
 	}
 
 	public void shoot() {
@@ -68,22 +69,22 @@ public class IO {
 	}
 
 	public void openFlaps() {
-		left.set(DoubleSolenoid.Value.kForward);
-		right.set(DoubleSolenoid.Value.kForward);
+		leftFlapPiston.set(DoubleSolenoid.Value.kForward);
+		rightFlapPiston.set(DoubleSolenoid.Value.kForward);
 	}
 
 	public void closeFlaps() {
-		left.set(DoubleSolenoid.Value.kReverse);
-		right.set(DoubleSolenoid.Value.kReverse);
+		leftFlapPiston.set(DoubleSolenoid.Value.kReverse);
+		rightFlapPiston.set(DoubleSolenoid.Value.kReverse);
 	}
 
 	public void update() {
         double speed = currentState == IOMotorState.FORWARD ? motorSpeed : currentState == IOMotorState.REVERSE ? -motorSpeed : 0.0;
 
-        leftWall.set(-speed);
-        leftFlap.set(-speed);
-        rightWall.set(speed);
-        rightFlap.set(speed);
+        leftWallMotor.set(-speed);
+        leftFlapMotor.set(-speed);
+        rightWallMotor.set(speed);
+        rightFlapMotor.set(speed);
 	}
 }
 
